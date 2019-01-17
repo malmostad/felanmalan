72a4e62305086e765479b1b30914bd306911f9df
Timo Engelhardt <timoengelhardt@timos-mbp.surf.wifi.malmo.se>
2019-01-17 14:25:15 +0100

    add additional info to Redux

--- client/src/Info.js
+++ client/src/Info.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Form, Input, Button } from "antd";
+import { connect } from "react-redux";
+import { reportAdd } from "./redux/actions";
 
 const { TextArea } = Input;
 
@@ -8,19 +10,36 @@ class Info extends Component {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
+        debugger;
+        this.props.reportAdd(values);
         console.log("Received values of form: ", values);
       }
     });
   };
 
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
       <Form onSubmit={this.handleSubmit} className="login-form">
         <Form.Item label="Email">
-          <Input />
+          {getFieldDecorator("email", {
+            rules: [
+              {
+                type: "email",
+                message: "The input is not valid E-mail!"
+              }
+            ]
+          })(<Input />)}
         </Form.Item>
         <Form.Item label="Additional Information about the Report">
-          <TextArea autosize={{ minRows: 4, maxRows: 6 }} />
+          {getFieldDecorator("message")(
+            <TextArea autosize={{ minRows: 4, maxRows: 6 }} />
+          )}
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Add
+          </Button>
         </Form.Item>
       </Form>
     );
@@ -29,4 +48,7 @@ class Info extends Component {
 
 const WrappedInfo = Form.create({ name: "info" })(Info);
 
-export default WrappedInfo;
+export default connect(
+  undefined,
+  { reportAdd }
+)(WrappedInfo);
