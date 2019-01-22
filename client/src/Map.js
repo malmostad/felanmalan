95965eb76fe7c796f02d569b8b83219cc6624183
Timo Engelhardt <timoengelhardt@Timos-MacBook-Pro.local>
2019-01-22 12:01:14 +0100

    add pointer when searching for location

--- client/src/Map.js
+++ client/src/Map.js
@@ -12,14 +12,14 @@ class Map extends Component {
   componentDidMount() {
     // is there a better way to do this?
     const props = this.props;
-    this.map = new mapboxgl.Map({
+    let map = new mapboxgl.Map({
       container: this.mapContainer,
       style: "mapbox://styles/mapbox/streets-v9",
       center: [13.003365, 55.6051458],
       zoom: 13
     });
 
-    this.map.addControl(
+    map.addControl(
       new mapboxgl.GeolocateControl({
         positionOptions: {
           enableHighAccuracy: true
@@ -30,7 +30,7 @@ class Map extends Component {
         props.reportAdd({ coordinates: { longitude, latitude } });
       })
     );
-    this.map.addControl(
+    map.addControl(
       // localize https://www.mapbox.com/mapbox-gl-js/example/mapbox-gl-geocoder-limit-region/
       new MapboxGeocoder({
         accessToken: mapboxgl.accessToken,
@@ -40,9 +40,30 @@ class Map extends Component {
         const coords = event.result.geometry.coordinates;
         const longitude = coords[0];
         const latitude = coords[1];
+        this.map.getSource("single-point").setData(event.result.geometry);
         props.reportAdd({ coordinates: { longitude, latitude } });
       })
     );
+    map.on("load", function() {
+      map.addSource("single-point", {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: []
+        }
+      });
+
+      map.addLayer({
+        id: "point",
+        source: "single-point",
+        type: "circle",
+        paint: {
+          "circle-radius": 10,
+          "circle-color": "#007cbf"
+        }
+      });
+    });
+    this.map = map;
   }
   componentWillUnmount() {
     this.map.remove();
