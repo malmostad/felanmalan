01f591a22cb8c92d21c823bf03fd195dc1d5a79c
Jon Andersson <andersson.jon@gmail.com>
2019-06-11 15:08:26 +0200

    add `.editorconfig` and add shortcut to lint from cli.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { connect } from "react-redux";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import App from "./App";
@@ -9,6 +8,11 @@ const store = createStore(() => {});
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
