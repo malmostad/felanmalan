062854ada85af27a6668dfc51581d831d27ef6a7
Jon Andersson <andersson.jon@gmail.com>
2019-06-11 14:05:37 +0200

    update so first basic test passes.

---
+++ client/__mocks__/mapbox-gl.js
@@ -0,0 +1,21 @@
+class Map {
+    on() {}
+    addControl() {}
+    remove() {}
+}
+class Marker {
+    on() {}
+}
+class NavigationControl {
+    on() {}
+}
+class GeolocateControl {
+    on() {}
+}
+
+export default {
+    GeolocateControl,
+    Map,
+    Marker,
+    NavigationControl,
+}
