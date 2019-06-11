97e7d514065724351df5b53eff56b7410dd3a662
Jon Andersson <andersson.jon@gmail.com>
2019-06-11 16:06:06 +0200

    bump `react-scripts` and add some config for no collide with test.

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,5 +1,21 @@
 const CracoAntDesignPlugin = require("craco-antd");
+const CracoLessPlugin = require("craco-less");
+module.exports = { plugins: [ { plugin: CracoLessPlugin, options: { lessLoaderOptions: { modifyVars: { "@primary-color": "#1DA57A", "@link-color": "#1DA57A", "@border-radius-base": "2px" }, javascriptEnabled: true } } } ] };
 
 module.exports = {
-  plugins: [{ plugin: CracoAntDesignPlugin }]
+    plugins: [
+        {
+            plugin: CracoAntDesignPlugin
+        },
+        {
+            plugin: CracoLessPlugin
+        }
+    ],
+  jest: {
+      configure: {
+          "moduleNameMapper": {
+              "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+          }
+      }
+  }
 };
