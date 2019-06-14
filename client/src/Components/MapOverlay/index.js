883046f88544dd8db2425d6c2c49b70e74862918
Jon Andersson <andersson.jon@gmail.com>
2019-06-14 17:10:00 +0200

    Fix Overlay Component. Break out title and logo to own component later on.

--- client/src/Components/MapOverlay/index.js
+++ client/src/Components/MapOverlay/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import "./MapOverlay.css";
+import styles from "./MapOverlay.module.css";
+import Logo from "Images/logo.svg";
+
 class MapOverlay extends Component {
   onClick = () => {
     const { onClick = false } = this.props;
@@ -11,46 +13,19 @@ class MapOverlay extends Component {
   render() {
     return (
       <div
-        style={styles.background}
+        className={styles.mapOverlay}
         onClick={this.onClick}
         onTouchStart={this.onClick}
       >
-        <h1 style={styles.title}>
-          <strong style={styles.titleStrong}>Markera ut platsen </strong>
-          där du vill rapportera ett fel
-        </h1>
+        <div className="screenTitleHolder">
+          <img src={Logo} alt="Malmö stad" />
+          <h1 className="screenTitle">
+            <strong className="boldText">Markera ut platsen </strong>
+            där du vill rapportera ett fel
+          </h1>
+        </div>
       </div>
     );
   }
 }
-const styles = {
-  background: {
-    position: "fixed",
-    display: "flex",
-    zIndex: 99,
-    top: 0,
-    bottom: 0,
-    width: "100%",
-    backgroundColor: "rgba(6, 107, 53, 0.85)",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  header: {
-    textAlign: "center",
-    padding: "60px 20px 20px"
-  },
-  title: {
-    maxWidth: "20rem",
-    fontWeight: "lighter",
-    letterSpacing: "0.2px",
-    textAlign: "center",
-    color: "#FFE5E6",
-    fontSize: "47px",
-    lineHeight: "50px"
-  },
-  titleStrong: {
-    fontWeight: "bold"
-  }
-};
-// add loading of state here
 export default MapOverlay;
