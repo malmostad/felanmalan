321ed651e90322994796864d7dd32be06ed07319
Jon Andersson <andersson.jon@gmail.com>
2019-07-02 15:34:34 +0200

    quick update o last page.

--- client/src/Screens/Done/index.js
+++ client/src/Screens/Done/index.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { clear } from "redux/actions";
 import styles from "./Done.module.css";
+import LargeHeader from "Components/LargeHeader";
 import ScreenTitle from "Components/ScreenTitle";
 import PhotoItem from "Components/PhotoItem";
 
@@ -44,15 +45,17 @@ class Done extends Component {
     const { description = "" , address = "" } = this.state;
     return (
       <div>
-        <ScreenTitle
-          strongTextLast={true}
-          titleStrong="skickats"
-          title="Felanmälan har " // intentionally trailing whitespace
-        >
-          <p className={styles.doneDescription}>
-            Din felanmälan är nu skickad och vi kommer att se över din rapport så snart som möjligt. Om du uppgav din mail i förra steget så kommer du att få ett mail men en länk till ditt ärende.
-          </p>
-        </ScreenTitle>
+        <LargeHeader>
+          <ScreenTitle
+            strongTextLast={true}
+            titleStrong="skickats"
+            title="Felanmälan har " // intentionally trailing whitespace
+          >
+            <p className={styles.doneDescription}>
+              Din felanmälan är nu skickad och vi kommer att se över din rapport så snart som möjligt. Om du uppgav din mail i förra steget så kommer du att få ett mail men en länk till ditt ärende.
+            </p>
+          </ScreenTitle>
+        </LargeHeader>
         <div className="content">
           <div className={styles.doneCard}>
             <label>Ärende #number</label>
