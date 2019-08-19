0869753448bbcd8810f501ade0dc1de58a43d421
Jon Andersson <andersson.jon@gmail.com>
2019-08-19 11:24:46 +0200

    Adding Texts as reducer and start moving out texts to the reducer for now.

---
+++ client/src/redux/reducers/texts.js
@@ -0,0 +1,12 @@
+const initialState = {
+  accept: "Acceptera",
+  introPageTitle: "Malmö stads felanmälan",
+  introPageIngress: "Här anmäler du fel på Malmös gator, torg och parker.",
+  introPageText:
+    "Vi vill att tjänsten ska fungera så bra som möjligt för dig som besökare. För att kunna ta reda på vad vi kan göra bättre analyserar vi hur våra besökare använder tjänsten med webbkakor (cookies)"
+};
+const textsReducer = (state = initialState, action) => {
+  return state;
+};
+
+export default textsReducer;
