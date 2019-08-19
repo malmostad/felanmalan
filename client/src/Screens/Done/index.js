a4a5d9aaa612ccea540f8c20cafa3fb07be76b83
Jon Andersson <andersson.jon@gmail.com>
2019-08-19 17:18:58 +0200

    Fix new done page.

--- client/src/Screens/Done/index.js
+++ client/src/Screens/Done/index.js
@@ -1,76 +1,32 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
 import { clear } from "redux/actions";
 import styles from "./Done.module.css";
-import LargeHeader from "Components/LargeHeader";
-import ScreenTitle from "Components/ScreenTitle";
-import PhotoItem from "Components/PhotoItem";
+import NextButton from "Components/NextButton";
+import FullScreenTitle from "Components/FullScreenTitle";
 
-const MAX_IMAGE_WIDTH_PERCENT = 100;
 class Done extends Component {
-  state = {
-    description: "",
-    address: "",
-    previews: []
-  };
   componentDidMount() {
-    const { report, clear } = this.props;
-    this.setState({
-      ...report
-    });
+    const { clear } = this.props;
     clear();
   }
-  renderImages = () => {
-    const { previews = [] } = this.state;
-    if (previews.length === 0) {
-      return null;
-    }
-    const imageWidth = MAX_IMAGE_WIDTH_PERCENT / previews.length;
-    return (
-      <div className={styles.imageHolder}>
-        {previews.map(preview => (
-          <PhotoItem
-            style={{ width: `${imageWidth}%` }}
-            key={preview.uuid}
-            preventRemove={true}
-            previewDataURL={preview.previewDataURL}
-            uuid={preview.uuid}
-          />
-        ))}
-      </div>
-    );
-  };
   render() {
-    const { description = "" , address = "" } = this.state;
+    const { texts = {} } = this.props;
     return (
-      <div>
-        <LargeHeader>
-          <ScreenTitle
-            strongTextLast={true}
-            titleStrong="skickats"
-            title="Felanmälan har " // intentionally trailing whitespace
-          >
-            <p className={styles.doneDescription}>
-              Din felanmälan är nu skickad och vi kommer att se över din rapport så snart som möjligt. Om du uppgav din mail i förra steget så kommer du att få ett mail men en länk till ditt ärende.
-            </p>
-          </ScreenTitle>
-        </LargeHeader>
-        <div className={`content ${styles.doneCard}`}>
-          <label>Ärende #number</label>
-          {this.renderImages()}
-          {address && <p className={styles.address}>{address}</p>}
-          <p styles={styles.description}>{description}</p>
+      <div className={styles.donePage}>
+        <div>
+          <FullScreenTitle titleStrong={texts.donePageTitle} />
+          <p className={styles.text}>{texts.donePageText}</p>
         </div>
-        <Link className={styles.button} to="/">Börja om</Link>
+        <NextButton inverted={true} text={texts.createNew} to="/" />
       </div>
     );
   }
 }
 
 function mapStateToProps(state = {}) {
-  const { report = {} } = state;
-  return { report };
+  const { texts } = state;
+  return { texts };
 }
 
 export default connect(
