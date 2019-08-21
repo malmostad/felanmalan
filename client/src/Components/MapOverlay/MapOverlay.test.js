89fe444ce9f183b161e2fb9f9074c42495727e70
Jon Andersson <andersson.jon@gmail.com>
2019-08-21 16:24:44 +0200

    Remove unused files.

--- client/src/Components/MapOverlay/MapOverlay.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import MapOverlay from "./";
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<MapOverlay />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
