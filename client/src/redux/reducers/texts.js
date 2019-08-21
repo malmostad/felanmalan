e94803f1681396094c578f2716e86190ca1b6b2f
Jon Andersson <andersson.jon@gmail.com>
2019-08-21 13:31:53 +0200

    Update texts

--- client/src/redux/reducers/texts.js
+++ client/src/redux/reducers/texts.js
@@ -1,16 +1,50 @@
+import React from "react";
 const initialState = {
   skip: "Hoppa över",
   accept: "Acceptera",
   addPhoto: "Lägg till bild",
-  addPhotoOnProblemAndPlace: "Lägg till foton på problemet & platsen",
+  addPhotoOnProblemAndPlace: [
+    "Lägg till ",
+    <strong>bilder</strong>,
+    " på ",
+    <strong>problemet</strong>,
+    " & ",
+    <strong>platsen</strong>
+  ],
   clickOrDragImage: "Klicka eller dra bild hit för att starta uppladdning",
-  introPageTitle: "Malmö stads felanmälan",
+  introPageTitle: [<strong>Malmö stads felanmälan</strong>],
   introPageIngress: "Här anmäler du fel på Malmös gator, torg och parker.",
   introPageText:
     "Vi vill att tjänsten ska fungera så bra som möjligt för dig som besökare. För att kunna ta reda på vad vi kan göra bättre analyserar vi hur våra besökare använder tjänsten med webbkakor (cookies)",
-  locationPageWhereIsTheError: "Vart är felet, placera det på kartan",
+  locationPageWhereIsTheError: [
+    <strong>Vart</strong>,
+    " är ",
+    <strong>felet</strong>,
+    ", ",
+    <strong>placera</strong>,
+    " det på ",
+    <strong>kartan</strong>
+  ],
   createNew: "Skapa ny",
-  donePageTitle: "Tack för din felanmälan",
+  descriptionTitle: <strong>Beskriv problemet</strong>,
+  description: "Beskrivning",
+  descriptionPlaceHolder: "Beskriv problemet du vill felanmäla...",
+  contactPageTitle: [
+    <strong>Följ</strong>,
+    " vad som ",
+    <strong>händer</strong>,
+    " med din ",
+    <strong>felanmälan</strong>
+  ],
+  emailOrPhone: "E-post eller telefonnummer",
+  emailOrPhonePlaceHolder: "Skriv din e-postadress eller ditt telefonnumer",
+  sendIssueReport: "Skicka in felanmälan",
+  donePageTitle: [
+    <strong>Tack</strong>,
+    " för din ",
+    <br />,
+    <strong>felanmälan</strong>
+  ],
   donePageText: "Den hjälper oss hålla vår vackra stad iordning."
 };
 const textsReducer = (state = initialState, action) => {
