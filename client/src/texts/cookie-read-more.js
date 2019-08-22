1f86e1349cb5ccdde89079e8f3ce9624696d36a4
Jon Andersson <andersson.jon@gmail.com>
2019-08-22 10:38:09 +0200

    Quick display of read more text

---
+++ client/src/texts/cookie-read-more.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const readMore = (
+  <>
+    <p>
+      Denna information finns också att läsa på sidan Om webbplatsen, som du når
+      via en länk i sidfoten.
+    </p>
+    <h2>Webbkakor (cookies) på malmo.se</h2>
+    <p>
+      På malmo.se använder vi webbkakor (cookies). En webbkaka är en liten textfil
+      med information som lagras på besökarens dator för att förbättra webbplatsen
+      för dig som besökare.
+    </p>
+    <p>
+      Vi använder webbkakor för att analysera hur webbplatsen används, genom att
+      webbkakorna till exempel håller ordning på hur snabb webbplatsen varit för
+      dig och vilka sidor som besökts.
+    </p>
+    <p>
+      Läs mer om webbkakor och vilka som används på malmo.se på sidan Om webbkakor
+      (cookies) som du når via en länk i sidfoten på <a href="https://malmo.se">malmo.se</a>
+    </p>
+    <h2>Personuppgifter på malmo.se</h2>
+    <p>
+      Ett IP-nummer är din unika adress på internet. IP-numret används för att
+      information som skickas över internet ska nå fram till rätt plats. Det
+      enskilda IP-numret är ointressant för oss och vi avidentifierar all
+      information i så stor utsträckning som möjligt. Den information vi har i
+      våra systemloggar sparas endast i syfte att webbplatsen ska fungera bra.
+    </p>
+    <p>
+      Om du fyller i formulär på malmo.se sparas den information du valt att uppge
+      i våra systemloggar, för att säkerställa att informationen når fram.
+      Information om hur dina uppgifter hanteras får du i samband med att du
+      använder det specifika formuläret.
+    </p>
+    <p>Informationen rensas från våra systemloggar var sjätte månad.</p>
+    <h2>Mer information om dina rättigheter</h2>
+    <p>
+      Dina rättigheter regleras i dataskyddsförordningen (DSF/GDPR).
+      <a href="https://www.datainspektionen.se/dataskyddsreformen/dataskyddsforordningen/"> Du kan läsa mer om förordningen på Datainspektionens webbplats.
+      </a>
+    </p>
+  </>
+);
+
+export default readMore;
