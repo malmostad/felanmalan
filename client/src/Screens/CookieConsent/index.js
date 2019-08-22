b351c33ec9200e18481c63450651585f10872632
Jon Andersson <andersson.jon@gmail.com>
2019-08-22 11:01:23 +0200

    Rename file.

--- client/src/Screens/CookieConsent/index.js
+++ client/src/Screens/CookieConsent/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import styles from "./Intro.module.css";
+import styles from "./CookieConsent.module.css";
 
 import { acceptCookies } from "redux/actions";
 
