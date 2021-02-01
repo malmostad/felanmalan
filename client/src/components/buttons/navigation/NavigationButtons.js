f4f8b1cce0b42b48a2c24b565217f4e2ecc664c4
Martin Gustavsson <Marreeeehh93@gmail.com>
2021-02-01 13:53:13 +0100

    fixed-change-navigation

--- client/src/components/buttons/navigation/NavigationButtons.js
+++ client/src/components/buttons/navigation/NavigationButtons.js
@@ -1,15 +1,30 @@
 import { ButtonWrapper } from '../index'
-import {formViews} from '../../../views/index'
+import { formViews } from '../../../views/index'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import { useUpdate } from '../../../contexts/UpdateContext'
 
 const NavigationButtons = () => {
+  const [disabledPrevious, setDisabledPrevious] = useState(false)
+  const [disabledNext, setDisabledNext] = useState(false)
+  const {
+    setNextView,
+    setCurrentView,
+    setPreviousView,
+    currentView,
+  } = useUpdate()
 
-  const [disabledPrevious, setDisabledPrevious] = useState(true);
-  const [disabledNext, setDisabledNext] = useState(false);
-  const { setNextView, setCurrentView, setPreviousView, currentView } = useUpdate();
+  useEffect(() => {
+    if (currentView + 1 >= formViews.length) {
+      setDisabledNext(true)
+      setDisabledPrevious(false)
+    } else if (currentView === 0) {
+      setDisabledNext(false)
+      setDisabledPrevious(true)
+    }
+  }, [currentView])
 
+  /*
   useEffect(() => {
     if(currentView && formViews.length) {
 
@@ -22,7 +37,8 @@ const NavigationButtons = () => {
 
   }, [currentView]);
 
-/*switch(expression) {
+*/
+  /*switch(expression) {
   case x:
     // code block
     break;
@@ -34,36 +50,37 @@ const NavigationButtons = () => {
 }  */
 
   const handleClickNext = () => {
-    setPreviousView(prevState => prevState);
-    setCurrentView(prevState => prevState +1);
-    setNextView(prevState => prevState -1)
-  };
+    setPreviousView((prevState) => prevState)
+    setCurrentView((prevState) => prevState + 1)
+    setNextView((prevState) => prevState - 1)
+  }
 
-  const handleClickPrevious= () => {
-    setPreviousView(currentView -1)
-    setCurrentView(currentView -1)
+  const handleClickPrevious = () => {
+    setPreviousView(currentView - 1)
+    setCurrentView(currentView - 1)
     setNextView(currentView)
-  };
+  }
 
   const handleSubmit = () => {
-    console.log("submit")
+    console.log('submit')
   }
 
   return (
     <ButtonWrapper>
       <ButtonWrapper.Container>
-      { 
-        !disabledNext &&
-        <ButtonWrapper.Button inverted onClick={!disabledNext ? handleClickNext : handleSubmit}>
-          {!disabledNext ? "Next" : "Submit"}
-        </ButtonWrapper.Button>
-      }
-      { 
-        !disabledPrevious &&
-        <ButtonWrapper.Button inverted onClick={handleClickPrevious}>
-          Previous
-        </ButtonWrapper.Button>   
-      }
+        {!disabledNext && (
+          <ButtonWrapper.Button
+            inverted
+            onClick={!disabledNext ? handleClickNext : handleSubmit}
+          >
+            {!disabledNext ? 'Next' : 'Submit'}
+          </ButtonWrapper.Button>
+        )}
+        {!disabledPrevious && (
+          <ButtonWrapper.Button inverted onClick={handleClickPrevious}>
+            Previous
+          </ButtonWrapper.Button>
+        )}
       </ButtonWrapper.Container>
     </ButtonWrapper>
   )
