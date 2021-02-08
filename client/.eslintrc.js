00b3df37737f6147ec5ac974e5f9741aa601ff75
Fredrik Widegren <Fredrik.widegren@Malmo.se>
2021-02-08 18:10:52 +0100

    removed unecessary dependecies

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,39 +1,22 @@
 module.exports = {
-  root: true, // Make sure eslint picks up the config at the root of the directory
-  parserOptions: {
-    ecmaVersion: 2020, // Use the latest ecmascript standard
-    sourceType: 'module', // Allows using import/export statements
-    ecmaFeatures: {
-      jsx: true, // Enable JSX since we're using React
-    },
-  },
-  settings: {
-    react: {
-      version: 'detect', // Automatically detect the react version
-    },
-  },
   env: {
     browser: true,
-    commonjs: true,
-    es6: true,
-    jest: true,
+    es2021: true
   },
   extends: [
-    'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
   ],
   rules: {
-    'react/display-name': 'off',
-    'react/prop-types': 'off',
-    'no-console': 'warn',
-    'react/react-in-jsx-scope': 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-      },
-    ],
-  },
-};
+  }
+}
--- client/src/views/UploadImageView/forms/ImageUploadForm.js
+++ client/src/views/UploadImageView/forms/ImageUploadForm.js
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import { useReport } from '../../../contexts/ReportContext'
+import { useState } from 'react';
+import { useReport } from '../../../contexts/ReportContext';
 
 const ImageUploadForm = () => {
-  const { report, setReport } = useReport()
-  const [image, setImage] = useState({})
+  const { report, setReport } = useReport();
+  const [image, setImage] = useState({});
 
   return (
     <form>
       <input type="file" name="imageUploadForm" id="1" />
     </form>
-  )
-}
+  );
+};
 
-export default ImageUploadForm
+export default ImageUploadForm;
