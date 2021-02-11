a45b2304750e34f692b35a50e0e6d11ef0405fad
Filip Sjöberg <filip.sjoberg@hotmail.com>
2021-02-11 13:28:40 +0100

    adding form to the description

--- client/src/views/Description/DescriptionView.js
+++ client/src/views/Description/DescriptionView.js
@@ -1,10 +1,28 @@
+import { useEffect } from 'react'
+import { useReport } from '../../contexts/ReportContext'
+
 const DescriptionView = () => {
+  const { report, setReport } = useReport()
+
+  const handelDescriptionChange = (e) => {
+    setReport((prevReport) => ({ ...prevReport, info: { description: e.target.value } }))
+  }
+
+  useEffect(() => {
+    console.log('our report', report)
+  }, [report])
+
   return (
     <>
       <form>
         <label>
           Beskrivning
-          <textarea type="text" name="description" />
+          <textarea
+            type="text"
+            name="description"
+            placeholder="Beskriv problemet du vill felanmäla"
+            onChange={handelDescriptionChange}
+          />
         </label>
       </form>
     </>
