51f5de754928c89d5b1fb4a9543265b9f21bf435
Filip Sjöberg <filip.sjoberg@hotmail.com>
2021-02-11 16:25:20 +0100

    adding dummy map-view that generate lat long when click button

--- client/src/views/Map/MapView.js
+++ client/src/views/Map/MapView.js
@@ -1,8 +1,35 @@
+import { useEffect } from 'react'
+import { useReport } from '../../contexts/ReportContext'
+import BgMapView from './map-view.png'
+import styled from 'styled-components/macro'
+const MapContainer = styled.div`
+  background-image: url(${BgMapView});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 200px;
+  width: 100%;
+`
 const MapView = () => {
+  const { report, setReport } = useReport()
+
+  const coordinates = {
+    lat: '55.59705552372533',
+    lng: '12.977538048269887',
+  }
+
+  const handleLongLat = () => {
+    setReport((prevReport) => ({ ...prevReport, location: coordinates }))
+  }
+
+  useEffect(() => {
+    console.log(report)
+  }, [report])
+
   return (
-    <>
-      <h1>Map</h1>
-    </>
+    <MapContainer>
+      <button onClick={handleLongLat}>Min position</button>
+    </MapContainer>
   )
 }
 
