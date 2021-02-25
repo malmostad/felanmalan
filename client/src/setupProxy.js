539c5717707a7d9b0343b5edf9aa807c6dfa02f6
Filip Sjöberg <filip.sjoberg@hotmail.com>
2021-02-25 14:13:39 +0000

    Add backend proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
-  app.use('/photos', createProxyMiddleware({ target: 'http://localhost:3001/' }))
-  app.use('reports', createProxyMiddleware({ target: 'http://localhost:3001/' }))
+  app.use('/photos', createProxyMiddleware({ target: process.env.REACT_APP_RAILS_URL }))
+  app.use('/reports', createProxyMiddleware({ target: process.env.REACT_APP_RAILS_URL }))
 }
