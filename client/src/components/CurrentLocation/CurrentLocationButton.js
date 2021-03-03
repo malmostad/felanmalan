b09fe8698f48b498a4ecab5eefa32b26b011bb36
Martin Gustavsson <Marreeeehh93@gmail.com>
2021-03-03 15:47:47 +0100

    added button that gets your position

--- client/src/components/CurrentLocation/CurrentLocationButton.js
+++ client/src/components/CurrentLocation/CurrentLocationButton.js
@@ -3,9 +3,19 @@ import { StyledFetchButton } from '../styles/buttons/Buttons'
 import { FiNavigation as NavIcon } from 'react-icons/fi'
 
 const CurrentLocationButton = () => {
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        console.log('Latitude: ' + pos.coords.latitude + 'Longitude: ' + pos.coords.longitude)
+      })
+    } else {
+      console.log('Geolocation is not supported by this browser.')
+    }
+  }
+
   return (
     <>
-      <StyledFetchButton>
+      <StyledFetchButton onClick={getLocation}>
         <NavIcon
           size="1.4rem"
           style={{ color: 'white', marginTop: '5px', transform: 'rotate(-20deg)' }}
