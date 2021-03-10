ffcac53de188a5ac9cf0ce2e71e449ec247c9f7b
Filip Sjöberg <filip.sjoberg@hotmail.com>
2021-03-10 09:13:09 +0000

    useReducer to the mapContext

--- client/src/components/mapBox/MapBox.js
+++ client/src/components/mapBox/MapBox.js
@@ -2,15 +2,21 @@ import ReactMapGl from 'react-map-gl'
 import './MapBox.css'
 import { FaMapPin as Marker } from 'react-icons/fa'
 import SearchBar from '../searchbar/SearchBar'
-import { useMap } from '../../contexts/MapContext'
+import { MapContext } from '../../contexts/MapContext'
+import { useContext } from 'react'
 
 const MapBox = () => {
-  const { viewport, setViewport } = useMap()
+  const { state, dispatch } = useContext(MapContext)
+  const { viewport } = state
+
+  const handelViewPortChange = (payload) => {
+    dispatch({ type: 'handelViewportChange', payload })
+  }
   return (
     <>
       <ReactMapGl
         {...viewport}
-        onViewportChange={(viewport) => setViewport(viewport)}
+        onViewportChange={(payload) => handelViewPortChange(payload)}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
         mapStyle="mapbox://styles/iandwe/cjxcy8xsy0h5f1cmrapgba9q0"
         width="100vw"
--- client/src/contexts/MapContext.js
+++ client/src/contexts/MapContext.js
@@ -1,26 +1,30 @@
-import { useContext, useState, createContext, useEffect, useReducer } from 'react'
+import { useReducer } from 'react'
+import { createContext } from 'react'
 
-const MapContext = createContext()
+export const MapContext = createContext()
 
-if (!MapContext) {
-  throw new Error('AppContext must be used with AppProvider!')
-}
-
-export const useMap = () => useContext(MapContext)
-
-export const MapProvider = ({ children }) => {
-  const [viewport, setViewport] = useState({
+const initialState = {
+  viewport: {
     latitude: 55.6051458,
     longitude: 13.003365,
     zoom: 13,
-  })
-  const maxBounds = [12.855952171065837, 55.49066310369751, 13.17594041283428, 55.6585718499375]
+  },
+  maxBounds: [12.855952171065837, 55.49066310369751, 13.17594041283428, 55.6585718499375],
+}
 
-  const mapValues = {
-    viewport,
-    setViewport,
-    maxBounds,
+const mapReducer = (mapState, { type, payload }) => {
+  let { viewport, zoom, maxBounds } = mapState
+
+  switch (type) {
+    case 'handelViewportChange':
+      return { ...mapState, viewport: payload }
+    default:
+      return { ...mapState }
   }
+}
 
-  return <MapContext.Provider value={mapValues}>{children}</MapContext.Provider>
+export const MapProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(mapReducer, initialState)
+
+  return <MapContext.Provider value={{ state, dispatch }}>{children}</MapContext.Provider>
 }
