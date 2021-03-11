138df94335891fc7066cf7959a0c8bef9e77f297
Martin Gustavsson <Marreeeehh93@gmail.com>
2021-03-11 15:23:32 +0100

    sending data long and lat to the report

--- client/src/components/mapBox/MapBox.js
+++ client/src/components/mapBox/MapBox.js
@@ -6,12 +6,16 @@ import { MapContext } from '../../contexts/MapContext'
 import { useContext } from 'react'
 import CurrentLocationButton from '../CurrentLocation/CurrentLocationButton'
 import { LoadingSpinner } from '../../components/loading/styles'
+import { useReport } from '../../contexts/ReportContext'
 
 const MapBox = () => {
+  const { handelSetFormInfo } = useReport()
   const { state, dispatch } = useContext(MapContext)
   const { viewport, userLocation, showPositionMarker, showLocationButton, isLoading } = state
 
-  const handelViewPortChange = (payload) => {
+  const handleViewPortChange = (payload) => {
+    handelSetFormInfo('longitude', payload.longitude)
+    handelSetFormInfo('latitude', payload.latitude)
     dispatch({ type: 'handleViewportChange', payload })
   }
 
@@ -19,7 +23,7 @@ const MapBox = () => {
     <>
       <ReactMapGl
         {...viewport}
-        onViewportChange={(payload) => handelViewPortChange(payload)}
+        onViewportChange={(payload) => handleViewPortChange(payload)}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
         mapStyle="mapbox://styles/iandwe/cjxcy8xsy0h5f1cmrapgba9q0"
         width="100vw"
