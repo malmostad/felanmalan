8beb7a6ed8026d1673fd854a1e6079162d7726bc
Martin Gustavsson <Marreeeehh93@gmail.com>
2021-03-30 14:22:48 +0200

    removed unnecessary files

--- client/src/views/ContactInfo/ContactInfoView.js
+++ client/src/views/ContactInfo/ContactInfoView.js
@@ -7,7 +7,6 @@ import {
   StyledInput,
   InputFormSecond,
 } from '../../components/styles/form/Form'
-import { useUpdate } from '../.././contexts/UpdateContext'
 
 const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 const mobilePattern = /^[0-9]{10}$/
