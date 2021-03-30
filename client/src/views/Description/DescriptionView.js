7596a349f4687cd25547258ae9eb472642e6bbfd
Martin Gustavsson <Marreeeehh93@gmail.com>
2021-03-30 15:46:13 +0200

    fixed style to description and validation

--- client/src/views/Description/DescriptionView.js
+++ client/src/views/Description/DescriptionView.js
@@ -1,6 +1,11 @@
 import { useEffect, useContext } from 'react'
 import { useReport } from '../../contexts/ReportContext'
 import { NavigationContext } from '../../contexts/NavigationContext'
+import {
+  StyledFormWrapper,
+  StyledTextArea,
+  DescriptionSpan,
+} from '../../components/styles/form/Form'
 
 const DescriptionView = () => {
   const { handelSetFormInfo, formState } = useReport()
@@ -15,19 +20,24 @@ const DescriptionView = () => {
     e.target.value ? dispatch({ type: 'enableNext' }) : dispatch({ type: 'disableNext' })
   }
 
+  console.log(formState.description, 'test description value')
+
   return (
     <>
-      <form>
-        <label>
-          Beskrivning
-          <textarea
+      <StyledFormWrapper>
+        <form>
+          <label htmlFor="Beskrivning">
+            Beskriv problemet du vill felanmäla
+            {!formState.description && <DescriptionSpan> * </DescriptionSpan>}
+          </label>
+          <StyledTextArea
             type="text"
             defaultValue={formState.description}
-            placeholder="Beskriv problemet du vill felanmäla"
+            placeholder="Beskrivning..."
             onChange={handleFormInfo}
           />
-        </label>
-      </form>
+        </form>
+      </StyledFormWrapper>
     </>
   )
 }
