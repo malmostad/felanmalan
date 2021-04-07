3d258a2247308f4c0167c407336bd16b7599e6f3
Filip Sjöberg <filip.sjoberg@hotmail.com>
2021-04-07 08:53:08 +0000

    try to solv bug whit button firefox

--- client/src/components/CurrentLocation/CurrentLocationButton.js
+++ client/src/components/CurrentLocation/CurrentLocationButton.js
@@ -5,36 +5,37 @@ import { MapContext } from '../../contexts/MapContext'
 
 const CurrentLocationButton = () => {
   const { dispatch, state } = useContext(MapContext)
-  const { showLocationButton } = state
-
-  const handleUserLocation = () => {
-    dispatch({ type: 'handleStartLoader' })
-    navigator.geolocation.getCurrentPosition((position) => {
-      const payload = {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-        zoom: 17,
-      }
-      dispatch({ type: 'handleUserLocation', payload })
-      dispatch({ type: 'handleShowPositionMarker' })
-      dispatch({ type: 'handleViewportChange', payload })
-      dispatch({ type: 'handleStopLoader' })
-    })
-  }
 
   useEffect(() => {
-    navigator.permissions.query({ name: 'geolocation' }).then((permissionStatus) => {
-      if (permissionStatus.state === 'denied') {
-        dispatch({ type: 'handleDisableButton' })
-      }
-      permissionStatus.onchange = () => {
-        if (permissionStatus.state === 'denied') {
-          dispatch({ type: 'handleDisableButton' })
-          dispatch({ type: 'handleStopLoader' })
+    if (window.navigator) {
+      navigator.permissions.query({ name: 'geolocation' }).then(function (permission) {
+        permission.state === 'denied' && dispatch({ type: 'handleDisableButton' })
+      })
+    }
+  }, [])
+
+  //check if user denied the location
+  const handleUserLocation = () => {
+    dispatch({ type: 'handleStartLoader' })
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const payload = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          zoom: 17,
         }
+        dispatch({ type: 'handleUserLocation', payload })
+        dispatch({ type: 'handleShowPositionMarker' })
+        dispatch({ type: 'handleViewportChange', payload })
+        dispatch({ type: 'handleStopLoader' })
+      },
+      (error) => {
+        dispatch({ type: 'handleDisableButton' })
+        dispatch({ type: 'handleStopLoader' })
       }
-    })
-  }, [showLocationButton])
+    )
+  }
 
   return (
     <>
