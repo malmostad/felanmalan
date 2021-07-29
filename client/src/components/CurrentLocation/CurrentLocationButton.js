3938ab837b78162e20212b6c95c99b855882248f
Timo Engelhardt <timo.engelhardt@gmail.com>
2021-07-29 17:23:02 +0200

    refactor MapView and MapView bug fixes

--- client/src/components/CurrentLocation/CurrentLocationButton.js
+++ client/src/components/CurrentLocation/CurrentLocationButton.js
@@ -1,66 +1,58 @@
-import { useContext, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { StyledFetchButton } from "../styles/buttons/Buttons";
 import { FiNavigation as NavIcon } from "react-icons/fi";
-import { MapContext } from "../../contexts/MapContext";
 import { LoadingSpinner } from "../styles/Spinners/Spinners";
 
-const CurrentLocationButton = () => {
-  const { state, dispatch } = useContext(MapContext);
-  const { isLoading } = state;
+const CurrentLocationButton = ({ onUserLocation }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEnabled, setIsEnabled] = useState(true);
 
   useEffect(() => {
     if (window.navigator.permissions) {
       window.navigator.permissions
         .query({ name: "geolocation" })
         .then(function (permission) {
-          permission.state === "denied" &&
-            dispatch({ type: "handleDisableButton" });
+          permission.state === "denied" && setIsEnabled(false);
         });
     }
   });
 
-  //check if user denied the location
-  const handleUserLocation = () => {
-    dispatch({ type: "handleStartLoader" });
+  const fetchUserLocation = () => {
+    setIsLoading(true);
 
     navigator.geolocation.getCurrentPosition(
       (position) => {
         const payload = {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
-          zoom: 17,
         };
-        dispatch({ type: "handleUserLocation", payload });
-        dispatch({ type: "handleShowPositionMarker" });
-        dispatch({
-          type: "handleViewportChange",
-          payload: { ...payload, transitionDuration: 2300 },
-        });
-        dispatch({ type: "handleStopLoader" });
+        onUserLocation(payload);
+        setIsLoading(false);
       },
       (error) => {
-        dispatch({ type: "handleDisableButton" });
-        dispatch({ type: "handleStopLoader" });
+        setIsEnabled(false);
       }
     );
   };
 
   return (
     <>
-      <StyledFetchButton onClick={handleUserLocation}>
-        {isLoading ? (
-          <LoadingSpinner />
-        ) : (
-          <NavIcon
-            size="1.4rem"
-            style={{
-              color: "white",
-              marginTop: "5px",
-              transform: "rotate(-20deg)",
-            }}
-          />
-        )}
-      </StyledFetchButton>
+      {isEnabled && (
+        <StyledFetchButton onClick={fetchUserLocation}>
+          {isLoading ? (
+            <LoadingSpinner />
+          ) : (
+            <NavIcon
+              size="1.4rem"
+              style={{
+                color: "white",
+                marginTop: "5px",
+                transform: "rotate(-20deg)",
+              }}
+            />
+          )}
+        </StyledFetchButton>
+      )}
     </>
   );
 };
--- client/src/views/Map/MapView.js
+++ client/src/views/Map/MapView.js
@@ -1,12 +1,164 @@
-import MapBox from "../../components/mapBox/MapBox";
-import { MapProvider } from "../../contexts/MapContext";
+import { useState, useEffect } from "react";
+import ReactMapGl, {
+  Marker,
+  NavigationControl,
+  FlyToInterpolator,
+  WebMercatorViewport,
+} from "react-map-gl";
+import CurrentLocationButton from "../../components/CurrentLocation/CurrentLocationButton";
+import SearchBar from "../../components/searchBar/SearchBar";
+import { useReport } from "../../contexts/ReportContext";
+import "./MapView.css";
+import { fetchAddressMapBoxAPI } from "../../api/api";
+import { ReactComponent as MarkerIcon } from "./pin.svg";
+
+const DEFAULT_VIEWPORT = {
+  latitude: 55.6051458,
+  longitude: 13.003365,
+  transitionDuration: 0,
+  zoom: 13,
+};
+const MAX_BOUNDS = [
+  [12.855952171065837, 55.49066310369751],
+  [13.17594041283428, 55.6585718499375],
+];
 
 const MapView = () => {
+  const { handelSetFormInfo, formState } = useReport();
+  const [address, setAddress] = useState("");
+  const [userLocation, setUserLocation] = useState();
+  const [renderPrefix, setRenderPrefix] = useState(false);
+  const [viewport, setViewport] = useState(DEFAULT_VIEWPORT);
+
+  useEffect(() => {
+    const { latitude, longitude, address } = formState;
+    if (latitude && longitude) {
+      setViewport({
+        ...viewport,
+        latitude,
+        longitude,
+      });
+      setAddress(address);
+    }
+  }, []);
+
+  const isCoordinatesOutOfBounds = (coordinates, maxBounds) => {
+    const [[swLng, swLat], [neLng, neLat]] = maxBounds;
+    const [longitude, latitude] = coordinates;
+    return (
+      longitude < swLng ||
+      longitude > neLng ||
+      latitude < swLat ||
+      latitude > neLat
+    );
+  };
+
+  const isOutOfBounds = (bounds, maxBounds) => {
+    const [sw, ne] = bounds;
+    return (
+      isCoordinatesOutOfBounds(sw, maxBounds) ||
+      isCoordinatesOutOfBounds(ne, maxBounds)
+    );
+  };
+
+  const onViewportChange = (nextViewport) => {
+    const bounds = new WebMercatorViewport(nextViewport).getBounds();
+    if (isOutOfBounds(bounds, MAX_BOUNDS)) {
+      return;
+    }
+    setViewport(nextViewport);
+    handelSetFormInfo("longitude", viewport.longitude);
+    handelSetFormInfo("latitude", viewport.latitude);
+  };
+
+  const transitionEnd = () => {
+    setViewport({
+      ...viewport,
+      transitionDuration: 0,
+    });
+  };
+
+  const onZoom = (nextViewport) => {
+    const bounds = new WebMercatorViewport(nextViewport).getBounds();
+    if (isOutOfBounds(bounds, MAX_BOUNDS)) {
+      return;
+    }
+    setViewport({ ...nextViewport, transitionDuration: 400 });
+  };
+
+  const onUserLocation = (payload) => {
+    setUserLocation(payload);
+    handelSetFormInfo("longitude", payload.longitude);
+    handelSetFormInfo("latitude", payload.latitude);
+    setViewport({
+      ...viewport,
+      longitude: payload.longitude,
+      latitude: payload.latitude,
+      transitionDuration: 400,
+    });
+  };
+
+  const onResultSelect = (payload) => {
+    setRenderPrefix(false);
+    setAddress(payload.address);
+    setViewport({
+      ...viewport,
+      longitude: payload.longitude,
+      latitude: payload.latitude,
+      transitionDuration: 400,
+    });
+  };
+
+  const updateAddress = async () => {
+    const response = await fetchAddressMapBoxAPI(viewport);
+    const { address, number } = response;
+    const displayAddress = [address, number].join(" ").trim();
+    setRenderPrefix(true);
+    setAddress(displayAddress);
+    handelSetFormInfo("address", displayAddress);
+  };
+
   return (
     <>
-      <MapProvider>
-        <MapBox />
-      </MapProvider>
+      <div className="map-style">
+        <SearchBar
+          address={address}
+          renderPrefix={renderPrefix}
+          onResultSelect={onResultSelect}
+        />
+        <ReactMapGl
+          {...viewport}
+          transitionInterpolator={new FlyToInterpolator()}
+          onViewportChange={(payload) => onViewportChange(payload)}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+          mapStyle="mapbox://styles/iandwe/ck0i4nprb08w91cmkp1939q6z"
+          width="100vw"
+          height="100%"
+          onTransitionEnd={transitionEnd}
+          onMouseDown={transitionEnd}
+          onMouseUp={updateAddress}
+          onTouchStart={transitionEnd}
+          onTouchEnd={updateAddress}
+        >
+          <NavigationControl
+            showCompass={false}
+            onViewportChange={onZoom}
+            style={{ right: 30, bottom: 80 }}
+          />
+          <CurrentLocationButton onUserLocation={onUserLocation} />
+          {userLocation && (
+            <Marker
+              latitude={userLocation.latitude}
+              longitude={userLocation.longitude}
+              offsetLeft={-10}
+              offsetTop={-10}
+            >
+              <div className="pulsating-circle"></div>
+            </Marker>
+          )}
+          <MarkerIcon className="pin-marker" />
+        </ReactMapGl>
+      </div>
     </>
   );
 };
