83d332bd67d56d5bf67c45fd98db4a18cb3dc912
Timo Engelhardt <timo.engelhardt@gmail.com>
2021-09-08 11:45:15 +0200

    fix regex for phone number

--- client/src/views/ContactInfo/ContactInfoView.js
+++ client/src/views/ContactInfo/ContactInfoView.js
@@ -20,7 +20,7 @@ import { StyledBoldHeader } from "../../components/styles/Typography/Typography"
 import { useUpdate } from "../.././contexts/UpdateContext";
 
 const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const mobilePattern = /^[0-9]{10}$/;
+const mobilePattern = /((?:\+|00)[17](?: |\-)?|(?:\+|00)[1-9]\d{0,2}(?: |\-)?|(?:\+|00)1\-\d{3}(?: |\-)?)?(0\d|\([0-9]{3}\)|[1-9]{0,3})(?:((?: |\-)[0-9]{2}){4}|((?:[0-9]{2}){4})|((?: |\-)[0-9]{3}(?: |\-)[0-9]{4})|([0-9]{7}))/;
 
 const ContactInfoView = () => {
   const { setCurrentViewHeading } = useUpdate();
